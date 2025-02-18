2a6f7266beda7c960a9f5461de0119df5ee075b3
Denis Katasonov <diarusfoxs@gmail.com>
2025-02-18 12:52:18 +0200

    Add Student entity -Add CreateStudent method with verification.

---
+++ StudentJSONSerialisation/Entyties/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace StudentJSONSerialisation.Entyties
+{
+    class Student
+    {
+        public int id;
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        private static int _nextId = 1;
+
+        private Student(string firstName, string lastName, int age)
+        {
+            id = _nextId++;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public static Student? CreateStudent(string firstName, string lastName, int age)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Ім'я та прізвище не може бути порожньою або складатися лише з пробілів.");
+                return null;
+            }
+
+            return new Student(firstName, lastName, age);
+        }
+    }
+}
