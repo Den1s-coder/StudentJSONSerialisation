4c548770f2a80ad664509dee3d19a3d3b8d84913
Denis Katasonov <diarusfoxs@gmail.com>
2025-03-07 18:24:07 +0200

    Create Change Student window (with out realisation)

---
+++ StudentJSONSerialisation/View/ChangeStudentWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentJSONSerialisation.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeStudentWindow.xaml
+    /// </summary>
+    public partial class ChangeStudentWindow : Window
+    {
+        public ChangeStudentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
