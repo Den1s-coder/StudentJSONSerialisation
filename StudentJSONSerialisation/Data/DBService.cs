cb21a161f7fdc56ca83d033ebd962841fa3e0018
Denis Katasonov <diarusfoxs@gmail.com>
2025-04-30 16:42:11 +0300

    Add DBService

---
+++ StudentJSONSerialisation/Data/DBService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentJSONSerialisation.Новая_папка
+{
+    internal class DBService
+    {
+    }
+}
