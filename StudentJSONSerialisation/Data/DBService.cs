0de753d0e4917226dd8d35be90317b5ef44bf0ea
Denis Katasonov <diarusfoxs@gmail.com>
2025-05-05 12:00:15 +0300

    Realise save studednt and groups in DB +load students from DB

--- StudentJSONSerialisation/Data/DBService.cs
+++ StudentJSONSerialisation/Data/DBService.cs
@@ -5,18 +5,18 @@ namespace StudentJSONSerialisation.Data
 {
     public static class DBService
     {
-        
+        private static string _connectionString = "Data Source=DESKTOP-KNGCD9Q;Initial Catalog=StudentAndGroups;Integrated Security=True";
 
-        public static void AddStudent (Student student)
+        public static void AddStudent(Student student)
         {
-            using SqlConnection conn = new( _connectionString );
+            using SqlConnection conn = new(_connectionString);
 
             conn.Open();
             SqlCommand cmd = new($"INSERT INTO Students (FirstName, LastName, Age, GroupId) VALUES (@f, @l, @a, @g)", conn);
-            cmd.Parameters.AddWithValue("@f",student.FirstName);
-            cmd.Parameters.AddWithValue("@l",student.LastName);
-            cmd.Parameters.AddWithValue("@a",student.Age);
-            cmd.Parameters.AddWithValue("@g",student.Group?.ID ?? (object)DBNull.Value);
+            cmd.Parameters.AddWithValue("@f", student.FirstName);
+            cmd.Parameters.AddWithValue("@l", student.LastName);
+            cmd.Parameters.AddWithValue("@a", student.Age);
+            cmd.Parameters.AddWithValue("@g", student.Group?.ID ?? (object)DBNull.Value);
             cmd.ExecuteNonQuery();
         }
 
@@ -25,32 +25,60 @@ namespace StudentJSONSerialisation.Data
             using SqlConnection conn = new(_connectionString);
 
             conn.Open();
-            SqlCommand cmd = new("INSERT INTO Groups (Name) Values (@n)",conn);
-            cmd.Parameters.AddWithValue("@n",group.Name);
+            SqlCommand cmd = new("INSERT INTO Groups (Name) Values (@n)", conn);
+            cmd.Parameters.AddWithValue("@n", group.Name);
             cmd.ExecuteNonQuery();
         }
 
-        public static List<Student> LoadStudents()
+        public static void LoadData(out List<Student> students, out List<Group> groups)
         {
-            List<Student> students = new List<Student>();
-            using SqlConnection conn = new(_connectionString);
+            students = new();
+            groups = new();
 
+            Dictionary<int, Group> groupDict = new();
+
+            using SqlConnection conn = new(_connectionString);
             conn.Open();
-            SqlCommand cmd = new("Select s.Id, s.FirstName,s.LastName,s.Age,g.Id as Group.Id, g.Name FROM Students s LEFT JOIN Groups g ON s.GroupId = g.Id", conn);
+
+            SqlCommand cmd = new("SELECT s.Id, s.FirstName, s.LastName, s.Age, g.Id AS GroupId, g.Name AS GroupName " +
+                                 "FROM Students s LEFT JOIN Groups g ON s.GroupId = g.Id", conn);
             SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read()) 
+
+            while (reader.Read())
             {
                 Group? group = null;
-                if (!reader.IsDBNull(4))
+
+                if (!reader.IsDBNull(4)) 
                 {
-                    group = Group.CreateGroup(reader.GetString(5));
-                    group.ID = reader.GetInt32(4);
+                    int groupId = reader.GetInt32(4);
+                    string groupName = reader.GetString(5);
+
+                    if (!groupDict.ContainsKey(groupId))
+                    {
+                        group = Group.CreateGroup(groupName);
+                        group.ID = groupId;
+                        groupDict.Add(groupId, group);
+                    }
+
+                    group = groupDict[groupId];
                 }
 
-                var student = new Student(reader.GetString(1), reader.GetString(2), reader.GetInt32(3)) { id = reader.GetInt32(0),Group = group };
+                var student = new Student(
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetInt32(3))
+                {
+                    id = reader.GetInt32(0),
+                    Group = group
+                };
+
                 students.Add(student);
             }
-            return students;
-        } 
+
+            groups.AddRange(groupDict.Values);
+        }
+
+
+        //TODO Update student after added him to group
     }
 }
--- StudentJSONSerialisation/View/CreateGroupWindow.xaml.cs
+++ StudentJSONSerialisation/View/CreateGroupWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using StudentJSONSerialisation.Entyties;
+﻿using StudentJSONSerialisation.Data;
+using StudentJSONSerialisation.Entyties;
 using System.Windows;
 
 namespace StudentJSONSerialisation.View
@@ -28,6 +29,8 @@ namespace StudentJSONSerialisation.View
             }
 
             MainWindow.Groups.Add(Group.CreateGroup(name));
+
+            DBService.AddGroup(Group.CreateGroup(name));
         }
     }
 }
